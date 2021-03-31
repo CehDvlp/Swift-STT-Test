8357cc6efc423c52661faea6af193527ff36e120
CehDvlp <nyih1719@gmail.com>
2021-04-01 01:15:48 +0900

    Initial Commit

---
+++ SttTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SttTestApp
+//
+//  Created by 최은화 on 2021/04/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
